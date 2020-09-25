eaba797aef883778ebd93baa50c1356ee05633c2
LenonAbreu <71392139+LenonAbreu@users.noreply.github.com>
2020-09-24 21:37:26 -0300

    Add files via upload

---
+++ exercicios/variaveis_expressoes_declaracoes_funcoes_2_1.py
@@ -0,0 +1,19 @@
+largura = 17 
+altura = 12.0 
+delimitador = "." 
+
+n_1 = largura/2 # valor número, tipo float
+
+n_2 = largura/2.0 # valor número, tipo float
+
+n_3 = altura/3 # valor número, tipo float
+
+n_4 = 1 + 2 * 5 # valor número, tipo inteiro
+
+n_5 = delimitador*5 #valor letra, tipo string
+
+print(n_1)
+print(n_2)
+print(n_3)
+print(n_4)
+print(n_5)
